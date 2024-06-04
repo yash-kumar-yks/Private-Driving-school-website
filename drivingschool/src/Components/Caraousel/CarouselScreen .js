b6119e968969416704282f852f9a8248ab333a59
yash <yashsngh57@gmail.com>
2024-06-04 15:44:57 +0530

    Cities displayed in search menu

--- drivingschool/src/Components/Caraousel/CarouselScreen .js
+++ drivingschool/src/Components/Caraousel/CarouselScreen .js
@@ -25,18 +25,11 @@ const CarouselScreen = () => {
   
   }, [slide])
   
-  const nextSlide = () => {
-    setSlide(slide === data.length - 1 ? 0 : slide + 1);
-  };
 
-  const prevSlide = () => {
-      setSlide(slide === 0 ? data.length - 1 : slide - 1);
-   
-  };
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
+      
       {data.map((item, idx) => {
         return (
           
@@ -48,10 +41,7 @@ const CarouselScreen = () => {
           />
         );
       })}
-      <BsArrowRightCircleFill
-        onClick={nextSlide}
-        className="arrow arrow-right"
-      />
+      
       <span className="indicators">
         {data.map((_, idx) => {
           return (
@@ -61,7 +51,6 @@ const CarouselScreen = () => {
                 slide === idx ? "indicator" : "indicator indicator-inactive"
               }
               
-              onClick={() => setSlide(idx)}
             ></button>
           );
         })}
--- drivingschool/src/Components/Map/FindyourCity.js
+++ drivingschool/src/Components/Map/FindyourCity.js
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
   import SearchableDropdown from '../../UI/Dropdown';
   import './FindYourCity.css';
   import MapPhoto from "../../assets/search-bg.jpg";
+  import {
+    GetCountries,
+    GetState,
+    GetCity,
+    GetLanguages, 
+  } from "react-country-state-city";
+import { FaCommentsDollar } from 'react-icons/fa';
+
 const FindyourCity = () => {
-  const options = [
-    { value: 'new-york', label: 'New York' },
-    { value: 'los-angeles', label: 'Los Angeles' },
-    { value: 'chicago', label: 'Chicago' },
-    { value: 'houston', label: 'Houston' },
-    { value: 'phoenix', label: 'Phoenix' },
-    // Add more options as needed
-  ];
-  
+  const options = [];
+const [citiesNameList, setCitiesNameList] = useState([]);
+
+const fetchData = async () => {
+  try {
+    const result = await GetState(101);
+    const citiesData = await Promise.all(result.map((data) => GetCity(101, data.id)));
+    return citiesData;
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+};
+
+useEffect(() => {
+  const executeFunctions = async () => {
+    const data = await fetchData();
+    if (data) {
+      setCitiesNameList(data);
+    }
+  };
+
+  executeFunctions();
+}, []);
+
+useEffect(() => {
+  citiesNameList.forEach((cityData) => {
+    cityData.forEach((city) => {
+      options.push({ value: city.name, label: city.name });
+    });
+  });
+}, [citiesNameList]);
+
   const handleSelect = (selectedValue) => {
     alert(`Selected value: ${selectedValue}`);
   };
   return (
     <div className='FindYourCityDiv'>
     <div className='FindYorCityHeading'>We have come closer to you. Now locate us</div>
-    <div style={{color:"white"}}> Locate a driving school in your city</div>
+    <div style={{color:"white"}}> Locate a driving school in your city.</div>
     <div className='FindCityDropdown'>
     <SearchableDropdown options={options} onSelect={handleSelect}/>
     </div>
     </div>
-  )
+  );
 }
 
 export default FindyourCity
